3217b3393434dce7575596039664afa9447fb53a
Harishankar <charishankar30@gmail.com>
2025-08-12 23:54:23 +0530

    afa

---
+++ lib/common/splashscreen.dart
@@ -0,0 +1,17 @@
+import 'package:actapp/backend/api.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: API.appcolor,
+      )
+    );
+  }
+}
